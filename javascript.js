0fa6d5b2da5ed33b7ed1185db214f8e9a8bc362a
Jorge-VP <villarrealjorgej@gmail.com>
2024-09-24 12:27:28 -0400

    Adding user interface

--- javascript.js
+++ javascript.js
@@ -1,19 +1,16 @@
+// DOM
+const $container = document.querySelector("#container");
+const $rock = document.querySelector("#rock");
+const $paper = document.querySelector("#paper");
+const $scissors = document.querySelector("#scissors");
+const $outcomeDiv = document.querySelector(".outcome");
+
 // getComputerChoice
 
 const getComputerChoice = function () {
-    randomNumber = Math.floor(Math.random()*3);
-    if (randomNumber === 0) {
-        return "rock";
-    }
-    else if (randomNumber === 1) {
-        return "paper";
-    }
-    else{
-        return "scissors"
-    }    
+    const options = ["rock", "paper", "scissors"];
+    return options[Math.floor(Math.random()*options.length)];      
 }
-//console.log("COMPUTER:", getComputerChoice());
-
 //getHumanChoice
 
 const getHumanChoice = function () {
@@ -45,63 +42,142 @@ let computerScore = 0;
 
 // playRound
 
-const playRound = function () {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
+const playRound = function (humanChoice, computerChoice) {
+    //humanChoice = getHumanChoice();
+    //computerChoice = getComputerChoice();
     if (humanChoice === "rock") {
         if (computerChoice === "rock") {
-            return "This round was tied.";
+            const $p = document.createElement("p");
+            $p.innerText = "This round was tied.";
+            $outcomeDiv.appendChild($p);
         }
         else if (computerChoice === "paper") {
             computerScore += 1;
-            return "You loss! paper beats rock.";
+            const $p = document.createElement("p");
+            $p.innerText = "You loss! paper beats rock.";
+            $outcomeDiv.appendChild($p);
         }
         else {
             humanScore += 1;
-            return "You win! rock beats scissors.";
+            const $p = document.createElement("p");
+            $p.innerText = "You win! rock beats scissors.";
+            $outcomeDiv.appendChild($p);
         }
     }
     else if (humanChoice === "paper") {
         if (computerChoice === "rock") {
             humanScore += 1;
-            return "You win! paper beats rock.";
+            const $p = document.createElement("p");
+            $p.innerText = "You win! paper beats rock.";
+            $outcomeDiv.appendChild($p);
         }   
         else if (computerChoice === "paper") {
-            return "This roun was tied.";
+            const $p = document.createElement("p");
+            $p.innerText = "This roun was tied.";
+            $outcomeDiv.appendChild($p);
         }
         else {
             computerScore += 1;
-            return "You loss! scissors beats paper.";
+            const $p = document.createElement("p");
+            $p.innerText = "You loss! scissors beats paper.";
+            $outcomeDiv.appendChild($p);
         }    
     }
     else {
         if (computerChoice === "rock") {
             computerScore += 1;
-            return "You loss! rock beats scissors.";
+            const $p = document.createElement("p");
+            $p.innerText = "You loss! rock beats scissors.";
+            $outcomeDiv.appendChild($p);
         }
         else if (computerChoice === "paper") {
             humanScore += 1;
-            return "You win! scissors beats paper.";
+            const $p = document.createElement("p");
+            $p.innerText = "You win! scissors beats paper.";
+            $outcomeDiv.appendChild($p);
         }
         else {
-            return "This round was tied."
+            const $p = document.createElement("p");
+            $p.innerText = "This round was tied."
+            $outcomeDiv.appendChild($p);
         }
+    }
+}
+
+// check Winner
+const checkWinner = function(humanScore, computerScore) {
+    if(humanScore === 5) {
+        $rock.style.display = "none";
+        $paper.style.display = "none";
+        $scissors.style.display = "none";
+        const $resultDiv = document.createElement("div");
+        const $h2 = document.createElement("h2");
+        const $playAgain = document.createElement("button");
+        $playAgain.innerText = "Play Again";
+        $h2.innerText = `You won! ${humanScore} to ${computerScore}. Great Job.`;
+        $resultDiv.appendChild($h2);
+        document.body.appendChild($resultDiv);
+        document.body.appendChild($playAgain);
+        $playAgain.addEventListener("click", () => location.reload());
+    }
+    if (computerScore === 5 ) {
+        $rock.style.display = "none";
+        $paper.style.display = "none";
+        $scissors.style.display = "none";
+        const $resultDiv = document.createElement("div");
+        const $h2 = document.createElement("h2");
+        const $playAgain = document.createElement("button");
+        $playAgain.innerText = "Play Again";
+        $h2.innerText = `You lost! ${humanScore} to ${computerScore}. Good luck next time.`;
+        $resultDiv.appendChild($h2); 
+        document.body.appendChild($resultDiv);
+        document.body.appendChild($playAgain);
+        $playAgain.addEventListener("click", () => location.reload());
     }
 }
 //console.log(playRound());
 
 //playGame
-
+/*
 const playGame = function () {
     for (let i = 1; i <= 5; i++){
         let game = playRound();
-        console.log("Round: ", i, game);
+        console.log(`Round: ${i} ${game}`);        
+        console.log("You win: ", humanScore);
+        console.log("You loss: ", computerScore);
+        //console.log("Tied: ", 5-(humanScore+computerScore));
     }
-    console.log("You win: ", humanScore);
-    console.log("You loss: ", computerScore);
-    console.log("Tied: ", 5-(humanScore+computerScore));
+    if (humanScore === computerScore) {
+        console.log("TIED!");            
+    }
+    else if (humanScore > computerScore){
+        console.log("YOU WIN!");
+    }
+    else {
+        console.log("YOU LOSS!");
+    }
+
 }
 playGame();
-
+*/
+// Events
+$rock.addEventListener("click", () => {
+    compSelection = getComputerChoice();
+    humanSelection = "rock";
+    playRound(humanSelection, compSelection);
+    checkWinner(humanScore, computerScore);
+});
+$paper.addEventListener("click", () => {
+    compSelection = getComputerChoice();
+    humanSelection = "paper";
+    playRound(humanSelection, compSelection);
+    checkWinner(humanScore, computerScore);
+});
+$scissors.addEventListener("click", () => {
+    compSelection = getComputerChoice();
+    humanSelection = "scissros";
+    playRound(humanSelection, compSelection);
+    checkWinner(humanScore, computerScore);
+})
 
 
